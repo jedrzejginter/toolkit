08594817cdefe7d70b896930c8afecc42987caf2
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2021-01-30 00:17:31 +0100

    Add comment

--- lintstaged.js
+++ lintstaged.js
@@ -1,12 +1,10 @@
-const { existsSync } = require('fs');
-const { join } = require('path');
-
 module.exports = {
   // Written as function, because this is how we can skip passing filenames
   // to the command.
   'package.json': () => `node ${require.resolve('./scripts/exact-deps.js')}`,
 
   // Format JSON and markdown files.
+  // This includes 'prettier-plugin-package' for formatting package.json
   '*.{json,md}': 'prettier --write',
 
   // It's important not to use "yarn lint", because it will lint whole
