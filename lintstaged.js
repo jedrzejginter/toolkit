ba536fbad7b650d4989d183c24a232f4a5cb5515
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2021-01-30 01:26:02 +0100

    Formatting

--- lintstaged.js
+++ lintstaged.js
@@ -1,5 +1,7 @@
 module.exports = {
   'package.json': () => `node ${require.resolve('./scripts/exact-deps.js')}`,
+
   '*.{json,md}': 'prettier --write',
+
   '*.{ts,tsx,js,jsx}': `eslint --ext '.js,.jsx,.ts,.tsx' --fix`,
 };
