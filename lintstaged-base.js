ac5755a604250c3da145b5e3755afabf613810d9
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2021-01-31 21:51:56 +0100

    Add const.js

--- lintstaged-base.js
+++ lintstaged-base.js
@@ -1,4 +1,6 @@
+const { LINT_STAGED_PRETTIER, LINT_STAGED_ESLINT } = require('./const');
+
 module.exports = {
-  '*.{json,md}': 'prettier --write',
-  '*.{ts,tsx,js,jsx}': `eslint --ext '.js,.jsx,.ts,.tsx' --ignore-pattern '!.*.js' --fix`,
+  [LINT_STAGED_PRETTIER]: 'prettier --write',
+  [LINT_STAGED_ESLINT]: `eslint --ext '.js,.jsx,.ts,.tsx' --ignore-pattern '!.*.js' --fix`,
 };
--- lintstaged.js
+++ lintstaged.js
@@ -1,6 +1,8 @@
+const { LINT_STAGED_PACKAGE_JSON } = require('./const');
 const lintStagedBase = require('./lintstaged-base');
 
 module.exports = {
   ...lintStagedBase,
-  'package.json': () => `node ${require.resolve('./scripts/exact-deps.js')}`,
+  [LINT_STAGED_PACKAGE_JSON]: () =>
+    `node ${require.resolve('./scripts/exact-deps.js')}`,
 };
