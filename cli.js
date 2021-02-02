9a74710dd1de9346dbf1dbe68bfbc1f4e9835ef6
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2021-02-02 15:40:13 +0100

    Use different dirs

--- cli.js
+++ cli.js
@@ -325,4 +325,12 @@ const map = {
   };
 
   writeFileSync('package.json', JSON.stringify(pkgJson, null, 2), 'utf-8');
+
+  const lockFile =
+    answers.packager === 'npm' ? 'package-lock.json' : 'yarn.lock';
+
+  if (!existsSync(lockFile)) {
+    const lockFileContents = answers.packager === 'npm' ? '{}' : '';
+    writeFileSync(lockFile, lockFileContents, 'utf-8');
+  }
 })();
